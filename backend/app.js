d81edfc8175a43facc720ee32b80bcc350b4d758
Michael <michael511.mp@gmail.com>
2021-11-25 12:07:59 -0500

    add express + nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,42 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use((req, res) => {
-  console.log('Frist middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Acess-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res) => {
-  res.send('Hello from express!');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added sucessfully!'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "taewsdt",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
